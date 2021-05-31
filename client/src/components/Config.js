f8dae1aa64804c0451b5559802b2babe34b507e2
jaeeunhyun <rosa2070@naver.com>
2021-05-31 23:38:46 +0900

    fsfsdf

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '7c2c63e1d173936b9f674d664c485227';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
