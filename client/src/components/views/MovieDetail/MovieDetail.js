1b3dc6fe3a00c3ede41edda666d4ed36a1b4f01a
jaeeunhyun <rosa2070@naver.com>
2021-06-03 13:56:47 +0900

    sdfsvb

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from '../LandingPage/Sections/MainImage'
 import MovieInfo from './Sections/MovieInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
+
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
     const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
 
     useEffect(() => {
 
@@ -25,9 +30,14 @@ function MovieDetail(props) {
             .then(response => response.json())
             .then(response => {
                 console.log('responseForCrew', response)
+                setCasts(response.cast)
 
             })
     }, [])
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle)
+    }
 
 
     return (
@@ -55,12 +65,34 @@ function MovieDetail(props) {
                 {/* Actors Grid */}
 
                 <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                    <button> Toggle Actor View</button>
+                    <button onClick={toggleActorView}> Toggle Actor View</button>
                 </div>
+
+                {ActorToggle &&
+
+                    <Row gutter={[16, 16]}>
+
+                        {Casts && Casts.map((cast, index) => (
+                            <React.Fragment key={index}>
+                                <GridCards
+                                    image={cast.profile_path ?
+                                        `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    characterName={cast.name}
+                                />
+
+                            </React.Fragment>
+                        ))}
+
+                    </Row>
+
+                }
+
+
+
             </div>
 
         </div>
     )
 }
 
-export default MovieDetai
+export default MovieDetail
