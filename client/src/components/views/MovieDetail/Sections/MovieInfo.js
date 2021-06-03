46bbf17d238a91bbbccf07be071bfc30a9971426
jaeeunhyun <rosa2070@naver.com>
2021-06-03 13:25:45 +0900

    sdasd

---
+++ client/src/components/views/MovieDetail/Sections/MovieInfo.js
@@ -0,0 +1,23 @@
+import { Descriptions, Badge } from 'antd'
+import React from 'react'
+
+function MovieInfo(props) {
+
+    let { movie } = props;
+    return (
+        <Descriptions title="Movie Info" bordered>
+            <Descriptions.Item label="Title">{movie.original_title}</Descriptions.Item>
+            <Descriptions.Item label="release_data">{movie.release_data}</Descriptions.Item>
+            <Descriptions.Item label="revenue">{movie.revenue}</Descriptions.Item>
+            <Descriptions.Item label="runtime">{movie.runtime}</Descriptions.Item>
+            <Descriptions.Item label="vote_average" spane={2}>
+                {movie.vote_average}
+            </Descriptions.Item>
+            <Descriptions.Item label="vote_count">{movie.vote_count}</Descriptions.Item>
+            <Descriptions.Item label="status">{movie.status}</Descriptions.Item>
+            <Descriptions.Item label="popularity">{movie.popularity}</Descriptions.Item>
+        </Descriptions>
+    )
+}
+
+export default MovieInfo
